2310dc28b6b65dea52df8693ea285dce75108f8d
ja <maryla.sylwestrzak@gmail.com>
2023-02-01 18:57:58 +0100

    polaczone

--- ZPO_project/src/main/java/org/example/Main.java
+++ ZPO_project/src/main/java/org/example/Main.java
@@ -1,12 +1,17 @@
 package org.example;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.SQLException;
+
 public class Main {
+
     public static void main(String[] args) {
-        Methods methods = new Methods();
-
-        methods.userBuyer(methods.logIn());
-        methods.printProductsFromCategory();
-        methods.printProductInfo();
-
+        try {
+            Menu menu = new Menu();
+            menu.startMenu();
+        } catch (SQLException | ClassNotFoundException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
